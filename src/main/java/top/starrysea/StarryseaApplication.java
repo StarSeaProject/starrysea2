d326361474d0df738cae884160cb9e2771579fae
xiongjunlang <xiongjunlang@agree.com.cn>
2018-04-13 17:14:06 +0800

    修正启动类的类名

--- src/main/java/top/starrysea/StarryseaApplication.java
+++ src/main/java/top/starrysea/StarryseaApplication.java
@@ -3,20 +3,14 @@ package top.starrysea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import top.starrysea.kql.facede.KumaRedisDao;
-import top.starrysea.kql.facede.KumaRedisDaoImpl;
-import top.starrysea.kql.facede.KumaSqlDao;
-import top.starrysea.kql.facede.KumaSqlDaoImpl;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
-public class StarrtseaApplication {
+public class StarryseaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StarrtseaApplication.class, args);
+		SpringApplication.run(StarryseaApplication.class, args);
 	}
 }
--- src/test/java/top/starrysea/StarryseaApplicationTests.java
+++ src/test/java/top/starrysea/StarryseaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class StarrtseaApplicationTests {
+public class StarryseaApplicationTests {
 
 	@Test
 	public void contextLoads() {
