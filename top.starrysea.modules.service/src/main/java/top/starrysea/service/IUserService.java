f23432b98081db0ccfc37d6647065e1b1b07836a
KumaLoveliver <kuma_loveliver@163.com>
2019-04-02 00:00:37 +0800

    User模块重命名为Admin模块

--- top.starrysea.modules.service/src/main/java/top/starrysea/service/IUserService.java
+++
@@ -1,8 +0,0 @@
-package top.starrysea.service;
-
-import top.starrysea.common.ServiceResult;
-import top.starrysea.object.dto.Admin;
-
-public interface IUserService {
-	ServiceResult loginService(Admin admin);
-}
