96325192d5bdccc1d8ee2c1998d15015f1c5af3f
Shuhuai Cao <csh980717@outlook.com>
2019-04-03 21:03:25 +0800

    用户注册成功后向其发送邮件

---
+++ top.starrysea.modules.mail/src/main/java/top/starrysea/service/mail/impl/UserRegisterMailServiceImpl.java
@@ -0,0 +1,28 @@
+package top.starrysea.service.mail.impl;
+
+import org.springframework.stereotype.Service;
+import top.starrysea.common.Common;
+import top.starrysea.kql.entity.Entity;
+import top.starrysea.mail.Mail;
+import top.starrysea.object.dto.User;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service("userRegisterMailService")
+public class UserRegisterMailServiceImpl extends MailServiceImpl{
+    @Override
+    public void sendMailService(Entity entity) {
+        List<String> mailList = new ArrayList<>();
+        User user = (User) entity;
+        String content = MessageFormat.format(contentTemplate, user.getUsername());
+        mailList.add(user.getUserEmail());
+        mailCommon.send(new Mail(mailList, "星之海志愿者公会", content));
+    }
+
+    @Override
+    protected String getHtml() {
+        return Common.readEmailHtml("user_register_mail.html");
+    }
+}
