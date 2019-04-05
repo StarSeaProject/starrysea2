4ff235ffd610ad5ea2295ad744a3d0bd26e12b2c
NagisaMemo <lq664376661@163.com>
2019-04-05 21:20:12 +0800

    格式化代码

--- top.starrysea.modules.common/src/main/java/top/starrysea/object/view/in/UserForCheck.java
+++ top.starrysea.modules.common/src/main/java/top/starrysea/object/view/in/UserForCheck.java
@@ -7,20 +7,20 @@ import javax.validation.constraints.Size;
 
 public class UserForCheck {
 
-    @NotBlank(message = "邮箱不能为空")
-    @Size(max = 100, message = "邮箱长度不能超过100")
-    private String userEmail;
+	@NotBlank(message = "邮箱不能为空")
+	@Size(max = 100, message = "邮箱长度不能超过100")
+	private String userEmail;
 
-    public String getUserEmail() {
-        return userEmail;
-    }
+	public String getUserEmail() {
+		return userEmail;
+	}
 
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
 
-    public User toDTO() {
-        return new User.Builder().userEmail(userEmail).build();
-    }
+	public User toDTO() {
+		return new User.Builder().userEmail(userEmail).build();
+	}
 
 }
