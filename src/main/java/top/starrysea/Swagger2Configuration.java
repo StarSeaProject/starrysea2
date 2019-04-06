57e30434560aa013ff21097de4e505ba6a4268bc
KumaLoveliver <kuma_loveliver@163.com>
2019-04-06 13:17:37 +0800

    swagger只对开发环境有效

--- src/main/java/top/starrysea/Swagger2Configuration.java
+++ src/main/java/top/starrysea/Swagger2Configuration.java
@@ -2,6 +2,7 @@ package top.starrysea;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Profile("dev")
 public class Swagger2Configuration {
 
 	@Bean
