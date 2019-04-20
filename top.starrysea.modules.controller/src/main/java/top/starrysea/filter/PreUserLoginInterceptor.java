02e4a2a8e00fa3eff64f6c6e0fdc596fedff25f7
KumaLoveliver <kuma_loveliver@163.com>
2019-04-20 21:55:39 +0800

    上线完成

--- top.starrysea.modules.controller/src/main/java/top/starrysea/filter/PreUserLoginInterceptor.java
+++ top.starrysea.modules.controller/src/main/java/top/starrysea/filter/PreUserLoginInterceptor.java
@@ -10,12 +10,19 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class PreUserLoginInterceptor implements HandlerInterceptor {
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		HttpSession session = request.getSession();
 		if (session.getAttribute(USER_SESSION_KEY) == null) {
-			response.sendRedirect(request.getContextPath() + "/login");
+			int endIndex = request.getRequestURL().length() - request.getRequestURI().length() + 1;
+			String url = request.getRequestURL().substring(0, endIndex);
+			// 如果当前的请求是https请求,那么就要把url中的http改成https,否则nginx会报400
+			if (request.getServerPort() == 443) {
+				url = url.replace("http", "https");
+			}
+			response.sendRedirect(url + "login");
 			return false;
 		}
 		return true;
