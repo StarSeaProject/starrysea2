9208a963c0fc948438e0163c809e9d206a073c6d
Shuhuai Cao <csh980717@outlook.com>
2019-05-25 12:33:47 +0800

    Revert "将前端传入的blob格式图像更改为正确的后缀"
    This reverts commit 85f7057519e9a8ef1563ef87caf8a3c6ab082fbe.

--- top.starrysea.modules.file/src/main/java/top/starrysea/file/FileType.java
+++ top.starrysea.modules.file/src/main/java/top/starrysea/file/FileType.java
@@ -1,20 +1,10 @@
 package top.starrysea.file;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum FileType {
 
 	IMG("FFD8FF", "89504E47", "47494638"), PDF("255044462D312E");
 
 	private String[] magicNums;
-
-	private Map<String, String> fileTypeMap = new HashMap<String, String>() {{
-		put("FFD8FF", "jpg");
-		put("89504E47", "png");
-		put("47494638", "gif");
-		put("255044462D312E", "pdf");
-	}};
 
 	private FileType(String... magicNums) {
 		this.magicNums = magicNums;
@@ -27,14 +17,5 @@ public enum FileType {
 			}
 		}
 		return false;
-	}
-
-	public String getTrueFileType(String targetMagicNum) {
-		for (String magicNum : magicNums) {
-			if (targetMagicNum.startsWith(magicNum)) {
-				return fileTypeMap.get(magicNum);
-			}
-		}
-		return "blob";
 	}
 }
