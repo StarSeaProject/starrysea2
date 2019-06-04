266452cc50b911e863d9a9459a0f7b1e4c843728
NagisaMemo <lq664376661@163.com>
2019-06-04 18:29:10 +0800

    session使用Redis进行集中存储

--- src/main/java/top/starrysea/StarryseaApplication.java
+++ src/main/java/top/starrysea/StarryseaApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.data.redis.RedisFlushMode;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 30 * 60, redisFlushMode = RedisFlushMode.IMMEDIATE)
 public class StarryseaApplication {
 
 	public static void main(String[] args) {
